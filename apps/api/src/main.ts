600a0671b71e2f00f146cb419495a69f72e45816
Theodoro Dikuyama <theodiku99@gmail.com>
2026-05-20 21:46:41 -0500

    feat(api): enable CORS with credentials for admin origin

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,19 @@
-import 'reflect-metadata';
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
-import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import "reflect-metadata";
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import cookieParser from "cookie-parser";
+import { AppModule } from "./app.module";
+import { AllExceptionsFilter } from "./common/filters/http-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.enableCors({
+    origin: (process.env.ADMIN_ORIGIN ?? "http://localhost:5173").split(","),
+    credentials: true,
+  });
   await app.listen(Number(process.env.PORT ?? 3001));
 }
 bootstrap();
